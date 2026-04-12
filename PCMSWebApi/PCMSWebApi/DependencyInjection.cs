3d03f395de62ddc7ff4c8b2d70b7ff9e28674300
Omphemetse Tleane <omphemetse.tleane@concourse.co.za>
2026-04-12 20:33:53 +0200

    add di for all layers

---
+++ PCMSWebApi/PCMSWebApi/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using PCMS.Application;
+using PCMS.Domain;
+using PCMS.Infrastructure;
+
+namespace PCMS.Presentation
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationDI(this IServiceCollection services)
+        {
+            services.AddApplicationDI();
+            services.AddDomainDI();
+            services.AddInfrastructureDI();
+            return services;
+        }
+    }
+}
