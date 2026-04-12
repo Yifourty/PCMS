5b4f1fff12edbb9eb04cb5abdd305db097c1f2ea
Omphemetse Tleane <omphemetse.tleane@concourse.co.za>
2026-04-13 01:18:14 +0200

    Create SearchCache.cs

---
+++ PCMSWebApi/PCMS.Infrastructure/Caching/SearchCache.cs
@@ -0,0 +1,15 @@
+﻿using PCMS.Domain.Entities;
+
+namespace PCMS.Infrastructure.Caching
+{
+    public class SearchCache
+    {
+        private readonly Dictionary<string, IEnumerable<Product>> _cache = new();
+
+        public bool TryGet(string key, out IEnumerable<Product>? result)
+            => _cache.TryGetValue(key, out result);
+
+        public void Set(string key, IEnumerable<Product> data)
+            => _cache[key] = data;
+    }
+}
