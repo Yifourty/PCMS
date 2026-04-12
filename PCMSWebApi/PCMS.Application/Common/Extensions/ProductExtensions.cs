fd01a51da6c2533a313a009ec47ecfc4a2025142
Omphemetse Tleane <omphemetse.tleane@concourse.co.za>
2026-04-13 01:07:37 +0200

    add product extension

---
+++ PCMSWebApi/PCMS.Application/Common/Extensions/ProductExtensions.cs
@@ -0,0 +1,24 @@
+﻿using PCMS.Domain.Entities;
+
+namespace PCMS.Application.Common.Extensions
+{
+    public static class ProductExtensions
+    {
+        public static IEnumerable<Product> FilterByCategory(this IEnumerable<Product> products, Guid? categoryId)
+        {
+            if (categoryId == null) return products;
+            return products.Where(p => p.CategoryId == categoryId);
+        }
+
+        public static IEnumerable<Product> SearchByName(
+            this IEnumerable<Product> products,
+            string? search)
+        {
+            if (string.IsNullOrWhiteSpace(search))
+                return products;
+
+            return products.Where(p =>
+                p.Name.Contains(search, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
